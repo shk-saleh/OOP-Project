88c18c6b8e2411eef0989517c0b31487ed1c69ef
Muhammad Saleh <143253075+shk-saleh@users.noreply.github.com>
2024-05-30 10:32:54 -0700

    Add files via upload

---
+++ Car Rental System/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+omyCar ec1 = new EconomyCar("Alto","VXR", 2019, 1000, true, "Manual",true);
+        EconomyCar ec2 = new EconomyCar("Cutlus", "VXL", 2009, 1150, true, "automatic",true);
+
+        LuxuryCar lc1 = new LuxuryCar("Civic","Oriel", 2023, 2400, true, "automatic");
+        LuxuryCar lc2 = new LuxuryCar("Fortuner", "TZ", 2020, 4150, true, "automatic");
+
+        SUV suv1 = new SUV("Revo","4x4", 2021, 6000, true, "Manual");
+        SUV suv2 = new SUV("KIA Sorento", "MG", 2024, 5000, true, "automatic");
